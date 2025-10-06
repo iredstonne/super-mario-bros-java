f9952db68e255486c8ad472fb5fe627734bde0d2
iredstonne <iredstonne.dev@gmail.com>
2025-10-06 20:23:43 +0200

    Add define/drawSprite and define/drawTile methods

--- src/main/java/io/github/contrib/SpriteSheet.java
+++ src/main/java/io/github/contrib/SpriteSheet.java
@@ -10,19 +10,31 @@ public class SpriteSheet {
 
     private final Image source;
 
-    private final Map<String, Image> sprites;
+    private final Map<String, Sprite> sprites;
 
     public SpriteSheet(Image source) {
         this.source = source;
         this.sprites = new HashMap<>();
     }
 
-    public void define(String name, int x, int y, int width, int height) {
-        this.sprites.put(name, null);
+    public void defineTile(String spriteName, double tileX, double tileY) {
+        this.defineSprite(spriteName, tileX * 16, tileY * 16, 16, 16);
     }
 
-    public void draw(GraphicsContext context) {
-        context.drawImage(this.source, 0, 0);
+    public void defineSprite(String spriteName, double spriteX, double spriteY, double spriteWidth, double spriteHeight) {
+        this.sprites.put(spriteName, new Sprite(this.source, spriteX, spriteY, spriteWidth, spriteHeight));
+    }
+
+    public void drawTile(GraphicsContext context, String spriteName, double x, double y) {
+        this.drawSprite(context, spriteName, x * 16, y * 16, 16, 16);
+    }
+
+    public void drawSprite(GraphicsContext context, String spriteName, double x, double y, double width, double height) {
+        final Sprite sprite = this.sprites.get(spriteName);
+        if(sprite == null) {
+            throw new RuntimeException("Couldn't find sprite " + spriteName);
+        }
+        sprite.drawImage(context, x, y, width, height);
     }
 
 }
