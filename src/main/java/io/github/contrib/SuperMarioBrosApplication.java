967fdbc20d9e982a68ce81ee26a307a3533fba89
iredstonne <iredstonne.dev@gmail.com>
2025-10-06 19:04:06 +0200

    Add canvas to scene

--- src/main/java/io/github/contrib/SuperMarioBrosApplication.java
+++ src/main/java/io/github/contrib/SuperMarioBrosApplication.java
@@ -1,14 +1,23 @@
 package io.github.contrib;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class SuperMarioBrosApplication extends Application {
     @Override
     public void start(Stage stage) {
+        StackPane pane = new StackPane();
+        Canvas canvas = new Canvas();
+        canvas.widthProperty().bind(pane.widthProperty());
+        canvas.heightProperty().bind(pane.heightProperty());
+        pane.getChildren().add(canvas);
+        SuperMarioBrosGameLoop gameLoop = new SuperMarioBrosGameLoop(canvas);
         stage.setTitle("Super Mario Bros Java");
-        stage.setWidth(1280);
-        stage.setHeight(720);
+        stage.setScene(new Scene(pane, 1280, 720));
         stage.show();
+        gameLoop.start();
     }
 }
