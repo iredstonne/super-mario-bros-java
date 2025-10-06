e84f8fdb3f5f480f3e5049d3d3ceb57fef51a4a5
iredstonne <iredstonne.dev@gmail.com>
2025-10-06 19:44:38 +0200

    Load and draw sprite sheet image

--- src/main/java/io/github/contrib/SuperMarioBrosGameLoop.java
+++ src/main/java/io/github/contrib/SuperMarioBrosGameLoop.java
@@ -3,20 +3,20 @@ package io.github.contrib;
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 
 public class SuperMarioBrosGameLoop extends AnimationTimer {
     private final Canvas canvas;
     private final GraphicsContext context;
+    private final SpriteSheet tiles;
 
     public SuperMarioBrosGameLoop(Canvas canvas) {
         this.canvas = canvas;
         this.context = canvas.getGraphicsContext2D();
+        this.tiles = new SpriteSheet(ImageLoader.load("/assets/tiles.png"));
     }
 
     @Override
     public void handle(long now) {
-        this.context.setFill(Color.RED);
-        this.context.fillRect(0, 0, this.canvas.getWidth(), this.canvas.getHeight());
+        this.tiles.draw(this.context);
     }
 }
